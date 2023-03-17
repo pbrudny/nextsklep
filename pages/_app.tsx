3b0b5bb8ef0bd8c1cdd3cd5165ef7009a53e69ca
Piotr Brudny <pbrudny@gmail.com>
2023-03-17 19:06:00 +0100

    Tydzien 3: Pobieranie treści po stronie klienta * CSR * React Query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {QueryClient, QueryClientProvider} from 'react-query'
+
+const client = new QueryClient();
 
 // dynamiczny - tu się dzieje dużo
 export default function App({ Component, pageProps }: AppProps) {
   return <div>
-    <Component {...pageProps} />
+    // react-query ma cache'a, który jest dostępny
+    // dla wszystkich komponentów z drzewa
+    // dzieki temu m.in wykonanie tego samego zapytania
+    // wykonuje się tylko raz
+    <QueryClientProvider client={client}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   </div>
 }
