ef88e6a4ab35a6f63b1d45d165a683859ab7747d
Piotr Brudny <pbrudny@gmail.com>
2023-03-17 14:25:32 +0100

    Tydzien 3: Wyświetlanie kolekcji produktow przez SSG

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,9 +1,24 @@
 import {InferGetStaticPropsType} from "next";
+import {Product} from "@/components/Product";
 
 const ProductsPage = ({
   data,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return <div>{data[0].title}</div>
+  return <ul className={"grid grid-cols-1 sm:grid-cols-3 gap-4"}>
+    {data.map((product) => {
+      return <li key={product.id}>
+        <Product data = {
+          {
+           description: product.description,
+           thumbnailUrl: product.image,
+           thumbnailAlt: product.title,
+           rating: product.rating.rate,
+          }
+        }
+        />
+      </li>;
+    })}
+  </ul>
 };
 
 fetch('https://fakestoreapi.com/products/1')
@@ -13,20 +28,9 @@ fetch('https://fakestoreapi.com/products/1')
 
 export default ProductsPage;
 
-// w trybie produkcyjnym to się wykonuje raz w trakcie buildowania
-// aplikacji czyli ta funkcja już nie jest widoczna w przeglądarce
-// czyli Next sobie trzyma te propsy
-// w trybie developerskim dzieje się to przy każdej zmianie kodu
 export const getStaticProps = async () => {
   const res = await fetch(`https://fakestoreapi.com/products`)
   const data: StoreApiResponse[] = await res.json();
-
-  const inneDate = {
-    // tu można by pobierać inne dane o swoim kształcie
-    // getStaticProps nam by to scalił
-    // InferGetStaticProps by ogarnąć typy (kształt), który już by odbiegał
-    // od samego StoreApiRespons
-  }
 
   return {
     props: {
