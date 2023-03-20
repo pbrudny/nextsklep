6aecae172dd958aa956ad67181e47c5afbed2f91
Piotr Brudny <pbrudny@gmail.com>
2023-03-20 12:09:27 +0100

    Tydzien 4: Komponent Image // Image-Component // rozwiazujemy problem layout shift tego, że w trakcie ładowania strony // najpierw jest tekst a potem layout się rozsypuje jak się załaduje obrazek // // Jak mamy layout= responsive to możemy podać // width and height jako proporcje // ale wtedy obrazki będą rozciągnięte // objectFit (contain/cover) // contain pokazuje cały obrazek zachowując proporcję // cover zachowuje proporcje ale obcina obraz

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 interface ProductDetails {
   id: string;
@@ -18,16 +19,32 @@ interface ProductListItemProps {
   data: ProductListItem;
 };
 
+// Image-Component
+// rozwiazujemy problem layout shift tego, że w trakcie ładowania strony
+// najpierw jest tekst a potem layout się rozsypuje jak się załaduje obrazek
+//
+// Jak mamy layout= responsive to możemy podać
+// width and height jako proporcje
+// ale wtedy obrazki będą rozciągnięte
+// objectFit (contain/cover)
+// contain pokazuje cały obrazek zachowując proporcję
+// cover zachowuje proporcje ale obcina obrazek grunt zeby zakryć cały obszar
 export const ProductListItem = ({data}: ProductListItemProps) => {
   return (
     <>
-      <img
+      <div className={"bg-white p-4"}>
+      <Image
         src={data.thumbnailUrl}
         alt={data.thumbnailAlt}
+        layout={"responsive"}
+        width={16}
+        height={9}
+        objectFit={"contain"}
       />
       <Link href={`/products/${data.id}`}>
         <h2 className={"p-4 text-3xl font-bold"}>{data.title}</h2>
       </Link>
+      </div>
     </>
   )
 };
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+// dla bezpieczenstwa musimy podac url obrazkow
+// przy zmianie tego pliku trzeba restartować serwer
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["fakestoreapi.com"]
+  }
 }
 
 module.exports = nextConfig
